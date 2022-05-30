3d48b0cb32d546100e215ca5271bd485e1e8c2e8
Daniel Silva <dsilva@cocus.com>
2022-05-30 10:35:29 +0100

    fix production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiMovieURL: 'https://api.themoviedb.org/',
   apiMovieVersion: '3',
   apiMovieKey: 'aa27e5b6d3a6ff3e8d5c88d359d4a667'
